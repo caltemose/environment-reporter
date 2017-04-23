d488474a03cdce941a2a0f6c4155262bb49ea9ec
Chad Altemose <chad@chadzilla.com>
2017-04-23 11:05:29 -0400

    final code for 0.1.0 version

--- index.js
+++ index.js
@@ -1,48 +1,61 @@
-var path = require('path')
+// tessel-specific dependencies
 var tessel = require('tessel')
 var climateLib = require('climate-si7005')
 var ambientlib = require('ambient-attx4')
-var admin = require("firebase-admin")
+
+// non-tessel dependencies
+var path = require('path')
+var moment = require('moment')
+var firebase = require("firebase-admin")
+
+//
+// Configuration
+//
+
+// timing intervals for how often to report are in seconds
+var testingInterval = 5
+var liveInterval = 15*60
+
+// mode - affects interval timing, false disables firebase
+var testing = false
+
+
+//
+// Firebase configuration and initialization
+//
 
 // admin private key downloaded from Firebase console
 var serviceAccount = require(path.join(__dirname, "./firebase-admin-config.json"))
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://sizzling-torch-6298.firebaseio.com",
-    databaseAuthVariableOverride: {
-        // must use this user id or will not have write access to data
-        uid: "tessel-user"
-    }
-})
-
-var db = admin.database()
-var ref = db.ref("/restricted")
-ref.once("value", function(snapshot) {
-    console.log(snapshot.val())
-})
+if (!testing) {
+    firebase.initializeApp({
+        credential: firebase.credential.cert(serviceAccount),
+        databaseURL: "https://sizzling-torch-6298.firebaseio.com",
+        databaseAuthVariableOverride: {
+            // must use this user id or will not have write access to data
+            uid: "tessel-user"
+        }
+    })
+}
 
 
-return
+//
+// Tessel setup
+//
 
+var climate = climateLib.use(tessel.port['A'])
+var ambient = ambientlib.use(tessel.port['B'])
 
-
-var ambient = ambientlib.use(tessel.port['B']);
-var climate = climateLib.use(tessel.port['A']);
-
+// used by initialization to determine if modules are ready to report
 var climateReady = false
 var ambientReady = false
-
-var testing = false
-
-if (!testing) {
-    var init = firebase.initializeApp(firebaseConfig)
-}
 
 // reset LEDs
 tessel.led[0].off()
 tessel.led[2].off()
 tessel.led[3].off()
+
+// handle climate and ambient module 'ready' and 'error' events
 
 climate.on('ready', function () {
     climateReady = true
@@ -74,6 +87,8 @@ function startupCheck () {
 function initializeReporting () {
     tessel.led[0].off()
     setImmediate(function loop () {
+        // yes i'm reading both temperature units and
+        // yes this is a little callback hell. get over it.
         climate.readTemperature('c', function (err, tempC) {
             climate.readTemperature('f', function (err, tempF) {
                 climate.readHumidity(function (err, humid) {
@@ -86,7 +101,7 @@ function initializeReporting () {
                                 lightdata.toFixed(8),
                                 sounddata.toFixed(8)
                             )
-                            var intervalTime = testing ? 5*1000 : 15*60*1000
+                            var intervalTime = testing ? testingInterval*1000 : liveInterval*1000
                             setTimeout(loop, intervalTime)
                         });
                     });
@@ -97,18 +112,23 @@ function initializeReporting () {
 }
 
 function report (c, f, humid, light, sound) {
-    var now = new Date()
-    var key = '/tessel/environment/' + now.getTime()
+    // the date passed to the database is GMT
+    // the location of the Tessel is known and any
+    // date localization can be handled by the UI
+    // using this data
+    var now = moment()
+    var key = '/environment/data/' + now.unix()
     var data = {
         tempC: c,
         tempF: f,
         humid: humid,
         light: light,
         sound: sound,
-        date: now
+        date: now.format()
     }
     var update = {}
     update[key] = data
+
     if (testing) {
         console.log('testing', update)
     } else {
@@ -118,6 +138,7 @@ function report (c, f, humid, light, sound) {
         tessel.led[3].on()
     }
 }
+
 
 function onComplete (err) {
     if (err) {
